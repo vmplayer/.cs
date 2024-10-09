f8eea02b47e8bd4eb038fc1ce3e9609bff0b41d2
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-08 21:37:17 -0300

    Create conta.cs

---
+++ testes-simples/testes-basicos/conta.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class basico {
+
+    public static void Main (string[] args)
+    {
+        int numero1 = 34;
+        int numero2 = 64;
+
+        Console.WriteLine(numero1 + numero2);
+    }
+}
